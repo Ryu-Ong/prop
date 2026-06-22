04feda6ee3c01d3d82db9ccc4f886afbb861ef72
AirVeil <airveil951@gmail.com>
2026-06-22 14:44:46 -0500

    jjij

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 const SPEED = 400.0
 
+@onready var animated_sprite = $AnimatedSprite2D
+
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = direction * SPEED
