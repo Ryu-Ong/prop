a2decd07dd6969d7d2a797f22d2666d0857fe5fd
Ryu-Ong <demetrius.ong.2025@gmail.com>
2026-06-24 11:46:56 -0500

    just some stuff

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-const SPEED = 400.0
+const SPEED = 500.0
 
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = Input.get_vector("hunter_left", "hunter_right", "hunter_up", "hunter_down")
 	velocity = direction * SPEED
 	move_and_slide()
 	
