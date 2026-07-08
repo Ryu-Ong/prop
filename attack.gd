2bba68dc0ff29f127bd27b6b6d130be695df601f
AirVeil <airveil951@gmail.com>
2026-07-08 17:01:14 -0500

    weapon beta

--- attack.gd
+++ attack.gd
@@ -1,25 +1,33 @@
 extends Node2D
 
+var dir
+var pos
+var rota
 
 var canHit = true
-var baton = preload("res://baton.tscn")
+var baton_scene = preload("res://baton.tscn")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if Input.is_action_pressed("Baton"):
+		print("Baton")
 	pass # Replace with function body.
-
-func Fire():
-	if canHit == true:
-		var baton = baton.instantiate()
-		baton.dir=rotation
-		baton.pos= global_position
-		baton.rota=global_rotation
-		get_parent().add_child(baton)
-		
 
 
 func _physics_process(delta: float) -> void:
+	print("running")
 	look_at(get_global_mouse_position())
 	if Input.is_action_pressed("Baton"):
 		Fire()
+		print("fire")
+
+func Fire():
+	if canHit == true:
+		var baton = baton_scene.instantiate()
+		print("instantiated")
+		baton.dir = rotation
+		baton.global_position = global_position
+		baton.global_rotation = global_rotation
+		get_parent().add_child(baton)
+		
