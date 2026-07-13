cf9cf71b9315d1a71a43c468041d29d671592a16
AirVeil <airveil951@gmail.com>
2026-07-13 14:54:42 -0500

    baton

--- attack.gd
+++ attack.gd
@@ -1,33 +1,39 @@
 extends Node2D
 
-var dir
-var pos
-var rota
-
+@onready var baton = $Baton
+@onready var hitbox = $Baton/CollisionShape2D
 var canHit = true
-var baton_scene = preload("res://baton.tscn")
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
+	baton.visible = false 
+	baton.monitoring = false
+	hitbox.disabled = true
 	if Input.is_action_pressed("Baton"):
 		print("Baton")
 	pass # Replace with function body.
 
 
 func _physics_process(delta: float) -> void:
-	print("running")
+
 	look_at(get_global_mouse_position())
 	if Input.is_action_pressed("Baton"):
 		Fire()
 		print("fire")
 
 func Fire():
-	if canHit == true:
-		var baton = baton_scene.instantiate()
-		print("instantiated")
-		baton.dir = rotation
-		baton.global_position = global_position
-		baton.global_rotation = global_rotation
-		get_parent().add_child(baton)
+	if canHit:
+
+		canHit = false
+		hitbox.disabled = false
+		baton.visible = true
+		baton.monitoring = true
+		await get_tree().create_timer(0.5).timeout
+		baton.visible = false
+		baton.monitoring = false
+		hitbox.disabled = true
+		canHit = true
 		
